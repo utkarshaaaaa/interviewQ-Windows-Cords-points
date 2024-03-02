de0966ea7ef4100f9908edaf25a4370517745b16
utkarsh <vyasutkarsh11@gmail.com>
2024-03-02 23:19:40 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+
+  const[cords,setCords]=useState([])
+  const[height,setHeight]=useState(10)
+  const[width,setWidth]=useState(10)
+
+  const handleClick=(e)=>{
+    const{clientX,clientY}= e
+    setCords([...cords,{
+      x:clientX,
+      y:clientY,
+    }])
+
+    console.log(cords)
+
+
+  }
+
+  
+  const undo=()=>{
+    const updatedCords=[...cords]
+    updatedCords.pop()
+    setCords(updatedCords)
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <button onClick={undo}>Undo</button>
+
+     Height<button onClick={()=>{setHeight(height+1)}}>+</button><button onClick={()=>{setHeight(height-1)}}>-</button>
+     Width<button onClick={()=>{setWidth(width+1)}}>+</button><button onClick={()=>{setWidth(width-1)}}>-</button>
+
+    
+ 
+    <div className="App" onClick={(e)=>{handleClick(e)}}>
+   
+      {
+        cords.map((cords,indx)=>{
+          return <div className='cords' style={{
+            left : cords.x -5 +"px",
+            top:cords.y -10 + "px",
+            width: width +"px",
+           height: height+"px"
+          }}>
+            
+            
+          </div>
+        })
+      }
+
+      
+      
     </div>
+    
+    </>
   );
 }
 
